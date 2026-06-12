50ea50090de184a8c7f73295da75d7e15617e417
Niraj Gajurel <gajurel8765@gmail.com>
2026-06-12 21:06:35 +0545

    Mobile App APi Added

--- src/app.ts
+++ src/app.ts
@@ -5,16 +5,17 @@ import { errorMiddleware } from "./middlewares/error.middleware";
 
 const app = express();
 
-app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
+app.use(
+  cors({
+    origin: ["http://localhost:3000", "http://localhost:3001"],
+    credentials: true,
+  })
+);
+
 app.use(express.json());
 
-
-app.use(cors({
-  origin: "http://localhost:3000", // your frontend URL
-  credentials: true
-}));
-
 app.use("/api/users", userRoutes);
+
 app.use(errorMiddleware);
 
 export default app;
