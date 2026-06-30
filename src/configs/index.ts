c2d2054f50c24454659a68b3e74876d858fcd8fb
Niraj Gajurel <gajurel8765@gmail.com>
2026-06-30 22:58:15 +0545

    Add backend configuration files

---
+++ src/configs/index.ts
@@ -0,0 +1,9 @@
+export const CLIENT_URL: string =
+  process.env.CLIENT_URL || "http://localhost:3000";
+
+export const EMAIL_USER: string = process.env.EMAIL_USER || "";
+
+export const EMAIL_PASS: string = process.env.EMAIL_PASS || "";
+
+export const JWT_SECRET: string =
+  process.env.JWT_SECRET || "freshcart_secret_key";
